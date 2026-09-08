486d1712ae51eafed434b20304dcd593252521b6
Fathi_Heelo <s12323885@stu.najah.edu>
2026-09-08 06:32:26 +0300

    on

--- backend/app/config.py
+++ backend/app/config.py
@@ -6,7 +6,11 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 class Settings(BaseSettings):
     app_name: str = "DQ-LLM Evaluator"
     database_url: str = "sqlite:///" + (Path(__file__).resolve().parents[1] / "projects.db").as_posix()
-    cors_origins: list[str] = ["http://localhost:5173"]
+    cors_origins: list[str] = [ "http://localhost:5173",
+        "http://localhost:5174",
+        "http://localhost:5179"]
+    api_url: str = "https://researchtool-u9h2.onrender.com"
+   
 
     model_config = SettingsConfigDict(
         env_file=Path(__file__).resolve().parents[2] / ".env",
